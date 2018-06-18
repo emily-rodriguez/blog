111341f7c78dcf72d1654bab97bf6caf6fb05142
emily-rodriguez <emily.m.b.rodriguez@gmail.com>
2018-06-18 16:39:38 -0500

    updated PostController

--- src/main/java/com/codeup/blog/PostController.java
+++ src/main/java/com/codeup/blog/PostController.java
@@ -2,22 +2,34 @@ package com.codeup.blog;
 
 import com.sun.tracing.dtrace.ProviderAttributes;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class PostController {
 
     @GetMapping("/posts")
-    public @ResponseBody String getPosts(){
-        return "posts index page";
+    public String index(Model model){
+        List<Post> posts = new ArrayList<>();
+        Post post1 = new Post("Post 1", "Blah blah blah");
+        Post post2 = new Post("Post 2", "Blah blah blah");
+        posts.add(post1);
+        posts.add(post2);
+        model.addAttribute("posts", posts);
+        return "/posts/index";
     }
 
     @GetMapping("/posts/{id}")
-    public @ResponseBody String getPost(@PathVariable long id){
-        return String.format("view and individual post with id of %s", id);
+    public String getPost(@PathVariable long id, Model model){
+        Post post = new Post("Post", "Here's a post");
+        model.addAttribute("post", post);
+        return "/posts/show";
     }
 
     @GetMapping("/posts/{id}/edit")
