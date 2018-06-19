0f38bc9af8518c60e150614f91fb8099d44056da
emily-rodriguez <emily.m.b.rodriguez@gmail.com>
2018-06-19 12:01:35 -0500

    exercise complete, starting class review

--- src/main/java/com/codeup/blog/PostController.java
+++ src/main/java/com/codeup/blog/PostController.java
@@ -1,6 +1,5 @@
 package com.codeup.blog;
 
-import com.sun.tracing.dtrace.ProviderAttributes;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
