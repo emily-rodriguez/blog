55db30ed88bb3429b844ea7c7b1eb9aa5e698b34
emily-rodriguez <emily.m.b.rodriguez@gmail.com>
2018-06-25 10:39:59 -0500

    moves all fragments into one file, changed file paths in htmls

--- src/main/java/com/codeup/blog/controllers/HomeController.java
+++ src/main/java/com/codeup/blog/controllers/HomeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class HomeController {
 
-    @GetMapping("/")
+    @GetMapping("/home")
     public @ResponseBody String landingPage(){
         return "This is the landing page";
     }
