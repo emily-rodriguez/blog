4f6f7a5fad431b4dd47ceed691b7bee6e4ea3846
emily-rodriguez <emily.m.b.rodriguez@gmail.com>
2018-06-25 14:55:45 -0500

    built login page, users must be logged in to edit, delete or create a post

--- src/main/java/com/codeup/blog/controllers/UserController.java
+++ src/main/java/com/codeup/blog/controllers/UserController.java
@@ -2,12 +2,14 @@ package com.codeup.blog.controllers;
 
 import com.codeup.blog.models.User;
 import com.codeup.blog.repositories.UserRepository;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import sun.plugin.liveconnect.SecurityContextHelper;
 
 @Controller
 public class UserController {
@@ -31,5 +33,11 @@ public class UserController {
         user.setPassword(hash);
         usersDao.save(user);
         return "redirect:/login";
+    }
+
+    @GetMapping("/login")
+    public String showLoginForm(){
+//        User loggedInUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        return "users/login";
     }
 }
